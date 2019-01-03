d1581c24f8613bc45ff69363bc6feb96cac7dc7f
roland <roland.manangan@gamil.com>
2019-01-03 12:18:34 -1000

    done, need to work on variable naming to keep better track of inner vs outer forloops

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -4,12 +4,26 @@
  * @param {number} [width=depth] 
  * @returns {string}
  */
-function nestedForLoops (depth, width = depth) {
-  let result = "\n";
+function nestedForLoops(depth, width = depth) {
+	let result = "\n";
 
-  // Write code here
+	// Write code here
+	for (var b = 0; b < depth; b++) {
 
+		for (var a = 0; a < width; a++) {
+
+
+			result += "{x:" + a + ", y:" + b + "}";
+			if (a + 1 < width) {
+				result += ", "
+			} else {
+				result += "\n"
+			}
+		}
+	}
+	console.log(result);
+	return result;
 };
 
 // To see your console output outside the tests add function calls here.
-// console.log(nestedForLoops(4));
+// console.log(nestedForLoops(3));
