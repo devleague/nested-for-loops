a40a99089b900bd951c4d72278887318e91fe691
Brad McKinney <brad.mckin@gmail.com>
2019-01-03 11:26:09 -1000

    forgot to save change

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -23,5 +23,3 @@ function nestedForLoops(depth, width = depth) {
 
 // To see your console output outside the tests add function calls here.
 // console.log(nestedForLoops(4));
-'\n{x:0, y:0}, {x:1, y:0}, {x:2, y:0}\n{x:0, y:1}, {x:1, y:1}, {x:2, y:1}\n{x:0, y:2}, {x:1, y:2}, {x:2, y:2}\n'
-'\n{x:0, y:0}, {x:1, y:0}, {x:2, y:0}, {x:0, y:1}, {x:1, y:1}, {x:2, y:1}, {x:0, y:2}, {x:1, y:2}, {x:2, y:2}, '
