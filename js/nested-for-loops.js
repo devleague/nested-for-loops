2cd8b9b7c43438b766189b33d909ee6c721592ac
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-06 10:37:34 -1000

    commented changes made

--- js/nested-for-loops.js
+++ js/nested-for-loops.js
@@ -8,20 +8,18 @@ function nestedForLoops (depth, width = depth) {
   let result = "\n";
   // Write code here
 
-  for (i=0;i<depth; i++){
+  for (let i=0;i<depth; i++){
 
-    for (j = 0; j<width; j++){
+    for (let j = 0; j<width; j++){
       if (j+1 == width){
         result = result.concat ("{x:"+j +", y:"+i+"}");
       }else {
         result = result.concat ("{x:"+j +", y:"+i+"}, ");
 
       }
-      // console.log(j, i);
     }
 
     result = result.concat ("\n");
-    console.log(result);
   }
 
 return result;
